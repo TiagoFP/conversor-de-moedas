229b6766e1729fe9ac2ef14ff152cd58b8c8ed3a
TiagoFP <tiagopimenteldev@gmail.com>
2024-06-03 19:04:07 -0300

    Criação de classes

--- Conversao.java
+++ Conversao.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Conversao {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conversao {
+
+    //String endereco;
+    int opcao;
+
+    public Valor Converter(double valor) {
+        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/d550f7e12c5ed7ae88ad0efe/latest/USD" + opcao);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco).build();
+
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Opção Inválida");
+        }
+
+        //return new Gson().fromJson(response.body(), Valor.class);
+    }
 }
--- Valor.java
+++ Valor.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public record Valor() {
+public record Valor(double valor) {
 }
